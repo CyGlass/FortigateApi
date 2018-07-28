1450a0dcc24235db82a9eebe380a2a46bb5affd5
jbregman <jbregman@users.noreply.github.com>
2018-07-28 07:39:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
   name = 'FortigateApi',
-  packages = ['FortigateApi'], # this must be the same as the name above
+  packages=find_packages(),
   version = '0.1',
   description = 'Access Fortigate REST API in python',
   author = 'David Chayla',
